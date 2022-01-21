94fc7e61fca6d52b329ac62d8df7f95467b47608
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-01-21 23:40:50 +0100

    added hex array

--- app/index.ts
+++ app/index.ts
@@ -1,30 +1,35 @@
+import { hueToHex } from "blockly";
 import document from "document";
-import {emojis} from "./fitmoji";
+import {emojis, emojisHex} from "./fitmoji";
 
 const emojiCode = document.getElementById("emojiCode");
 const emoji = document.getElementById("emoji");
 const nextEmoji = document.getElementById("nextEmoji");
 
 
-// CLICK THROUGH EMOJIS
-const n = emojis.length;
-nextEmoji.text = "⏩";
 
+// CLICK THROUGH EMOJIS
+//const n = emojis.length;
+const n = emojisHex.length;
+nextEmoji.text = "⏩";
+console.log(n)
 let counter = 0;
 
-nextEmoji.onclick = (evt) => {
+nextEmoji.onclick = () => {
     let c = counter % n;
     
-    //assign values from array
-    emojiCode.text = emojis[c][0];
-    emoji.text = emojis[c][1];
-  
+    // assign values from array
+    // emojiCode.text = emojis[c][0];
+    // emoji.text = emojis[c][1];
+    
+    emojiCode.text = String(emojisHex[c]);//how can I have 0xA9 here instaed 169???
+    emoji.text = String.fromCharCode(emojisHex[c])
+   
+    
     counter++;
 };
-
-//writes directly from hex :)
-console.log(String.fromCharCode(0x5448))
-emoji.text = String.fromCharCode(0x2b55)
+// writes directly from hex :)
+//emoji.text = String.fromCharCode(0x231A)
 
 
 
