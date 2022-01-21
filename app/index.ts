4e42458a28a9aba38e3bec542dfd2d8e3d195b7d
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-01-21 21:35:34 +0100

    added polyfill to read from hex

--- app/index.ts
+++ app/index.ts
@@ -1,90 +1,33 @@
 import document from "document";
-import {emojis} from "./fitmoji"
+import {emojis} from "./fitmoji";
+
+const emojiCode = document.getElementById("emojiCode");
+const emoji = document.getElementById("emoji");
+const nextEmoji = document.getElementById("nextEmoji");
 
 
-import * as fs from "fs";
-let utf8_data = "JavaScript is da best 😍";
-fs.writeFileSync("utf8.txt", utf8_data, "utf-8");
+// CLICK THROUGH EMOJIS
+const n = emojis.length;
+nextEmoji.text = "⏩";
 
-console.log(utf8_data)
-let emoji = document.getElementById("emoji");
+let counter = 0;
 
-emoji.text =  "😄";
-emoji.text =  "❤️";//from emoji
-//emoji.text =  String.fromCodePoint(0x1F47B);
+nextEmoji.onclick = (evt) => {
+    let c = counter % n;
+    
+    //assign values from array
+    emojiCode.text = emojis[c][0];
+    emoji.text = emojis[c][1];
+  
+    counter++;
+};
 
-//@ts-ignore
-if (!String.fromCodePoint) (function(stringFromCharCode) {
-    var fromCodePoint = function(_) {
-      var codeUnits = [], codeLen = 0, result = "";
-      for (var index=0, len = arguments.length; index !== len; ++index) {
-        var codePoint = +arguments[index];
-        // correctly handles all cases including `NaN`, `-Infinity`, `+Infinity`
-        // The surrounding `!(...)` is required to correctly handle `NaN` cases
-        // The (codePoint>>>0) === codePoint clause handles decimals and negatives
-        if (!(codePoint < 0x10FFFF && (codePoint>>>0) === codePoint))
-          throw RangeError("Invalid code point: " + codePoint);
-        if (codePoint <= 0xFFFF) { // BMP code point
-          codeLen = codeUnits.push(codePoint);
-        } else { // Astral code point; split in surrogate halves
-          // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-          codePoint -= 0x10000;
-          codeLen = codeUnits.push(
-            (codePoint >> 10) + 0xD800,  // highSurrogate
-            (codePoint % 0x400) + 0xDC00 // lowSurrogate
-          );
-        }
-        if (codeLen >= 0x3fff) {
-          result += stringFromCharCode.apply(null, codeUnits);
-          codeUnits.length = 0;
-        }
-      }
-      return result + stringFromCharCode.apply(null, codeUnits);
-    };
-    try { // IE 8 only supports `Object.defineProperty` on DOM elements
-      Object.defineProperty(String, "fromCodePoint", {
-        "value": fromCodePoint, "configurable": true, "writable": true
-      });
-    } catch(e) {
-        //@ts-ignore
-      String.fromCodePoint = fromCodePoint;
-    }
-}(String.fromCharCode));
+//writes directly from hex :)
+console.log(String.fromCharCode(0x5448))
+emoji.text = String.fromCharCode(0x2b55)
 
-//emoji.text =  String.fromCharCode(0x1F47B);
 
-function convertFromHex(hex) {
-    var hex = hex.toString();//force conversion
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
 
-function convertToHex(str) {
-    var hex = '';
-    for(var i=0;i<str.length;i++) {
-        hex += ''+str.charCodeAt(i).toString(16);
-    }
-    return hex;
-}
-emoji.text =  convertFromHex(0x1F47B);
-emoji.text = "😍"
-emoji.text = "❤️"
-emoji.text = "❌"
-emoji.text = "⏩"
-
-console.log(emojis.length)
-
-let i: number = 0;
-setInterval(function() {
-
-    if (i < emojis.length) {
-      emoji.text = emojis[i]
-    }
-    else return;
-    i++;
-}, 1000);
     
    
 
