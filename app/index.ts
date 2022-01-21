3bada97fc2aa95ea3de31b5fd9be562c3592c391
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-01-22 00:47:03 +0100

    all available added to array

--- app/index.ts
+++ app/index.ts
@@ -1,15 +1,14 @@
-import { hueToHex } from "blockly";
-import document from "document";
-import {emojis, emojisHex} from "./fitmoji";
 
-const emojiCode = document.getElementById("emojiCode");
+import document from "document";
+import {emojisHex} from "./fitmoji";
+
+const emojiNumber = document.getElementById("emojiCode");
 const emoji = document.getElementById("emoji");
 const nextEmoji = document.getElementById("nextEmoji");
 const lastEmoji = document.getElementById("lastEmoji");
 
 
 // CLICK THROUGH EMOJIS
-//const n = emojis.length;
 const n = emojisHex.length;
 nextEmoji.text = "⏩";
 lastEmoji.text = "⏪";
@@ -17,32 +16,33 @@ console.log(n)
 let counter = 0;
 
 const assignEmoji = (c) => {
-  emojiCode.text = String(c);//how can I have 0xA9 here instaed 169???
-  emoji.text = String.fromCharCode(emojisHex[c])
-}
+  emojiNumber.text = String(c);//how can I have 0xA9 here instaed 169???
+  emoji.text = String.fromCharCode(emojisHex[c]);
+};
 
 nextEmoji.onclick = () => {
-  if(counter<n){
+  if(counter<n) {
     let c = counter % n;
     counter++;
     assignEmoji(c)
   } else {
     return;
-  }
+  };
 };
 
 lastEmoji.onclick = () => {
-  if(counter>0){
+  if(counter>0) {
     counter--;
     let c = counter % n;
     assignEmoji(c);
   } else {
     return;
-  }
+  };
 };
 
 //TODO check and understand the polyfill
 //TODO how to print hexcode in text-format??? like 0xblahblah?
+//TODO check why the others don't work. other format needed?
 
 
 
