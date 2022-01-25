2229e268376ffac86bf9c1f884cf60aa80872360
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-01-25 21:50:24 +0100

    types

--- app/index.ts
+++ app/index.ts
@@ -3,23 +3,23 @@
 import document from "document";
 import {emojisHex, shortKeys} from "./fitmoji";
 import { fixedFromCharCode } from "./polyfills/readUTF";
-import "./polyfills/codePointAt"
-import "./polyfills/fromCodePoint"
+import "./polyfills/codePointAt";
+import "./polyfills/fromCodePoint";
 //import "./polyfills/unorm"
 
 // containers
-const buttons = document.getElementById("buttons");
-const single = document.getElementById("single") as GraphicsElement;
-const multiview = document.getElementById("multiview") as GraphicsElement;
+const buttons = document.getElementById("buttons") as GroupElement;
+const single = document.getElementById("single") as GroupElement;
+const multiview = document.getElementById("multiview") as GroupElement;
 
 
 // search in containers
-const emojiNumber = single.getElementById("emojiCode");
-const emoji = single.getElementById("emoji");
-const viewButton = buttons.getElementById("viewButton");
-const modeText = buttons.getElementById("viewMode")
-const nextEmoji = buttons.getElementById("nextEmoji");
-const prevEmoji = buttons.getElementById("lastEmoji");
+const emojiNumber = single.getElementById("emojiCode") as TextElement;
+const emoji = single.getElementById("emoji") as TextElement;
+const viewButton = buttons.getElementById("viewButton") as RectElement;
+const modeText = buttons.getElementById("viewMode") as TextElement;
+const nextEmoji = buttons.getElementById("nextEmoji") as TextElement;
+const prevEmoji = buttons.getElementById("lastEmoji") as TextElement;
 
 
 // BUTTONS
@@ -29,34 +29,34 @@ prevEmoji.text = "⏪";
 // FUNCTIONS
 // Formats array items text to 0x00000
 let displayHex = (hex: number) => {
-  return `0x${("00000" +hex.toString(16)).slice(-5)}`
+  return `0x${(`00000${hex.toString(16)}`).slice(-5)}`
 };
 
 //ASSIGNS EMOJIS
 // single emoji
-const assignEmoji = (c: number) => { 
+const assignEmoji = (c: number) :void => { 
   emojiNumber.text = String(c);
   emojiNumber.text = `index: ${c}, (${displayHex(emojisHex[c])})`;
   emoji.text = fixedFromCharCode(emojisHex[c]);
 };
 
-const assignMulti = (factor: number) => {
+const assignMulti = (factor: number) :void => {
   // 30 elements, load next 30 on click
   multiview.getElementsByClassName("multi").forEach((el: TextElement) => {
-    let index = Number(el.id)+(30*factor);
+    let index = Number(el.id) + (30 * factor);
     el.text = fixedFromCharCode(emojisHex[index]);
   }); 
 }
 
-const n = emojisHex.length;
-let counter = 0; // to switch to next item in single view
-let mode = 0; // to switch between single/multiview
-let factor = 0; // to multiply index for multiview
+const n: number= emojisHex.length;
+let counter: number = 0; // to switch to next item in single view
+let mode: number = 0; // to switch between single/multiview
+let factor: number = 0; // to multiply index for multiview
 
 
 // multi could be sufficient
 // SWITCHES VIEWMODE
-viewButton.onclick = () => {
+viewButton.onclick = () :void => {
   mode++;
   mode %=2;
   modeText.text = mode === 0 ? "single view" : "multi view";
@@ -69,7 +69,7 @@ viewButton.onclick = () => {
 
 function loopSymbols() {
   // browse to next
-  nextEmoji.onclick = () => {
+  nextEmoji.onclick = () :void => {
     if (mode === 0) { // single view
       if (counter < n) {
         counter++;
@@ -84,7 +84,7 @@ function loopSymbols() {
   };
   
   // browse to previous
-  prevEmoji.onclick = () => {
+  prevEmoji.onclick = () :void => {
     if (mode === 0) { // singleview
       if (counter > 0) {
         counter--;
@@ -101,29 +101,29 @@ function loopSymbols() {
       }
     };
   };
-}
+};
 
 // function for shortkeys
 // currently not in use
 const emo = (key: string) => {
-  let i= shortKeys.indexOf(key);
+  let i: number = shortKeys.indexOf(key);
   return fixedFromCharCode(emojisHex[i]);
-}
+};
 
 
 // CONSOLE.LOG CLICKED ITEM
 // then can copy into element.text
 
 // from multi view
-(document.getElementsByClassName("multi") as any).forEach((el) => {
-  el.onclick = () => {
+document.getElementsByClassName("multi").forEach((el: TextElement) => {
+  el.onclick = () :void => {
     console.log(JSON.stringify(el.text));
-  }  
+  };  
 });
 // from single view
-emoji.onclick = () => {
+emoji.onclick = () :void => {
   console.log(JSON.stringify(emoji.text));
-}  
+};  
 
 
 
@@ -136,8 +136,8 @@ emoji.onclick = () => {
 
 //TODO check logic in counters/clicks it#s somehow working, but not to 100% what I want
 
-console.log(String.fromCharCode(0x1f600))//  
-emoji.text = String.fromCharCode(0x1f600)//no glyph
-console.log(fixedFromCharCode(0x1f600))//������
+// console.log(String.fromCharCode(0x1f600))//  
+// emoji.text = String.fromCharCode(0x1f600)//no glyph
+// console.log(fixedFromCharCode(0x1f600))//������
 //String.fromCodePoint(0x1f600)//fromCodePoint not defined.... buhuhuuuuuu
 
